ebd29e7310caeddbb4a5d0652889680518c8fa0c
Josh <joshvelasqez6@gmail.com>
2020-07-31 20:28:50 -0600

    Imported code for javamusictag. Have the program update song information

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.util.Scanner;
 import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -9,6 +10,9 @@ import java.io.InputStream;
 import java.io.FileOutputStream;
 import java.io.File;
 import java.util.regex.Pattern;
+
+import org.farng.mp3.id3.ID3v2_3;
+
 import java.io.IOException;
 import java.io.ByteArrayOutputStream;
 import java.util.regex.Matcher;
@@ -28,27 +32,19 @@ public class Main {
         System.out.print("Enter filename: ");
         ArrayList<String> urls = getUrlsFromFile(userInput.nextLine());
         userInput.close();
-
-        // try {
-        // String id = getID("https://www.youtube.com/watch?v=l0U7SxXHkPY");
-        // String converter = loadConverter(id);
-        // String mp3url = getMP3URL(converter);
-        // try {
-        // downloadStreamData(mp3url, "test.mp3");
-        // } catch (Exception e) {
-
-        // }
-        // } catch (Exception e) {
-        // System.out.println("Failed");
-        // }
-    }
-
-    private static String getID(String youtubeUrl) {
-        Matcher m = VID_ID_PATTERN.matcher(youtubeUrl);
-        if (!m.find()) {
-            throw new IllegalArgumentException("Invalid YouTube URL.");
+        // For each url loop the following
+        for (String url : urls) {
+            try {
+                String id = getID(url);
+                String converter = loadConverter(id);
+                String mp3url = getMP3URL(converter); // Maybe get the song title from here
+                downloadStreamData(mp3url, "test.mp3");
+                // Hit the api to get song information
+                // updateInfo("C:\\Users\\joshv\\Desktop\\Github\\YoutubeConverter\\test.mp3");
+            } catch (Exception e) {
+                System.out.println("Failed");
+            }
         }
-        return m.group();
     }
 
     /**
@@ -69,72 +65,6 @@ public class Main {
         link = link.substring(6, link.length());
         link = link.substring(0, link.length() - 1);
         return link;
-    }
-
-    /**
-     * Uses 320youtube api to convert the youtube video
-     * 
-     * @param id
-     * @return
-     * @throws IOException
-     */
-    private static String loadConverter(String id) throws IOException {
-        String url = "https://www.320youtube.com/watch?v=" + id;
-        URL url2 = new URL(url);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        InputStream is = url2.openStream();
-        byte[] byteChunk = new byte[2500];
-        int n;
-        while ((n = is.read(byteChunk)) > 0) {
-            baos.write(byteChunk, 0, n);
-        }
-        is.close();
-        baos.flush();
-        baos.close();
-        byte[] bytes = baos.toByteArray();
-        return new String(bytes);
-    }
-
-    /**
-     * The commented code can be used to check the status of the download
-     * 
-     * @param url
-     * @param fileName
-     * @throws Exception
-     */
-    private static void downloadStreamData(String url, String fileName) throws Exception {
-        URL tU = new URL(url);
-        HttpURLConnection conn = (HttpURLConnection) tU.openConnection();
-
-        InputStream ins = conn.getInputStream();
-        FileOutputStream fout = new FileOutputStream(new File(fileName));
-        byte[] outputByte = new byte[4096];
-        int bytesRead;
-        // int length = conn.getContentLength();
-        // int read = 0;
-        while ((bytesRead = ins.read(outputByte, 0, 4096)) != -1) {
-            // read += bytesRead;
-            // System.out.println(read + " out of " + length);
-            fout.write(outputByte, 0, bytesRead);
-        }
-        fout.flush();
-        fout.close();
-    }
-
-    private static ArrayList<String> getUrlsFromFile(String filename) {
-        ArrayList<String> urls = new ArrayList<String>();
-        try {
-            FileInputStream fileInputStream = new FileInputStream(filename);
-            Scanner fileScanner = new Scanner(fileInputStream);
-            while (fileScanner.hasNextLine()) {
-                urls.add(fileScanner.nextLine());
-            }
-            fileInputStream.close();
-            fileScanner.close();
-        } catch (Exception e) {
-            System.out.println("Failed to read file.");
-        }
-        return urls;
     }
 
     // Hits the
@@ -172,4 +102,118 @@ public class Main {
         // Response response = client.newCall(request).execute();
     }
 
+    ///////////////////////////// WORKS ///////////////////////////////////////
+
+    /**
+     * Updates the information of the mp3 file based on the song info retrieved from
+     * API
+     * 
+     * @param songFileSource
+     * @param songInfo
+     */
+    private static void updateInfo(String songFileSource, Song songInfo) {
+        try {
+            RandomAccessFile file = new RandomAccessFile(songFileSource, "rw");
+            ID3v2_3 tag = new ID3v2_3(file);
+            tag.setSongTitle(songInfo.songTitle);
+            tag.setLeadArtist(songInfo.artist);
+            tag.setAlbumTitle(songInfo.albumTitle);
+
+            // Save changes to mp3 file
+            tag.write(file);
+        } catch (Exception e) {
+            System.out.println("Failed to update song information.");
+        }
+    }
+
+    /**
+     * Gets all the url links from file input
+     * 
+     * @param filename
+     * @return
+     */
+    private static ArrayList<String> getUrlsFromFile(String filename) {
+        ArrayList<String> urls = new ArrayList<String>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(filename);
+            Scanner fileScanner = new Scanner(fileInputStream);
+            while (fileScanner.hasNextLine()) {
+                urls.add(fileScanner.nextLine());
+            }
+            fileInputStream.close();
+            fileScanner.close();
+        } catch (Exception e) {
+            System.out.println("Failed to read file.");
+        }
+        return urls;
+    }
+
+    /**
+     * Gets the YouTube's ID of the url
+     * 
+     * @param youtubeUrl
+     * @return
+     */
+    private static String getID(String youtubeUrl) {
+        Matcher m = VID_ID_PATTERN.matcher(youtubeUrl);
+        if (!m.find()) {
+            throw new IllegalArgumentException("Invalid YouTube URL.");
+        }
+        return m.group();
+    }
+
+    /**
+     * The commented code can be used to check the status of the download
+     * 
+     * @param url
+     * @param fileName
+     * @throws Exception
+     */
+    private static void downloadStreamData(String url, String fileName) throws Exception {
+        URL tU = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) tU.openConnection();
+        InputStream ins = conn.getInputStream();
+        FileOutputStream fout = new FileOutputStream(new File(fileName));
+        byte[] outputByte = new byte[4096];
+        int bytesRead;
+        // int length = conn.getContentLength();
+        // int read = 0;
+        while ((bytesRead = ins.read(outputByte, 0, 4096)) != -1) {
+            // read += bytesRead;
+            // System.out.println(read + " out of " + length);
+            fout.write(outputByte, 0, bytesRead);
+        }
+        fout.flush();
+        fout.close();
+    }
+
+    /**
+     * Uses 320youtube api to convert the youtube video
+     * 
+     * @param id
+     * @return
+     * @throws IOException
+     */
+    private static String loadConverter(String id) throws IOException {
+        String url = "https://www.320youtube.com/watch?v=" + id;
+        URL url2 = new URL(url);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        InputStream is = url2.openStream();
+        byte[] byteChunk = new byte[2500];
+        int n;
+        while ((n = is.read(byteChunk)) > 0) {
+            baos.write(byteChunk, 0, n);
+        }
+        is.close();
+        baos.flush();
+        baos.close();
+        byte[] bytes = baos.toByteArray();
+        return new String(bytes);
+    }
+}
+
+class Song {
+    public String songTitle;
+    public String artist;
+    public String albumTitle;
 }
