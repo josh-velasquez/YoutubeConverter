ee6c9bddbae31fcf471fade410241aecf44b1e40
Josh <joshvelasqez6@gmail.com>
2020-10-19 18:06:35 -0600

    Fixed url extract bug. Added more features. Added instructions

--- YoutubeConverter/YoutubeConverter/MainWindow.xaml.cs
+++ YoutubeConverter/YoutubeConverter/MainWindow.xaml.cs
@@ -13,11 +13,20 @@ namespace YoutubeConverter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Song songInfo = null;
+        private string downloadUrl = null;
+        private string targetDir = null;
+
         public MainWindow()
         {
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
             InitializeComponent();
             SetDefaultDirectory();
+
+            // DEBUG
+            apiKeyTextBox.Text = "320d5e38afmsh4303ea9c1b8f26dp1ba115jsnee49a5333256";
+            targetDirTextBox.Text = "C:\\Users\\joshv\\Desktop\\Music";
+            urlTextBox.Text = "https://www.youtube.com/watch?v=jf0vrsP1i5g&ab_channel=TheGoodVibe";
         }
 
         /// <summary>
@@ -32,13 +41,14 @@ namespace YoutubeConverter
         /// <summary>
         /// Extracts the download url from the resulting html
         /// </summary>
-        /// <param name="html"></param>
+        /// <param name = "html" ></ param >
         /// <returns></returns>
         private string ExtractUrl(string html)
         {
             //Regex regex = new Regex("href=\"(https:\\/\\/s03.ytapivmp3.*?)\"");
+            //Regex regex = new Regex("(https:\\/\\/s02.ytapivmp3.*?)\"");
             Regex regex = new Regex("href=\"(.*?)\"");
-            return regex.Matches(html)[6].Groups[1].Value;
+            return regex.Matches(html)[7].Groups[1].Value;
         }
 
         /// <summary>
@@ -69,7 +79,7 @@ namespace YoutubeConverter
         /// <param name="message"></param>
         private void UpdateStatusUI(string message, bool failed = false)
         {
-            this.Dispatcher.Invoke(() =>
+            Dispatcher.Invoke(() =>
             {
                 statusListBox.Items.Add(new ListBoxItem { Content = message, Background = failed ? Brushes.Red : Brushes.LightGreen });
             });
@@ -81,15 +91,12 @@ namespace YoutubeConverter
         /// <param name="apikey">Key provided by the user (Shazam api key)</param>
         /// <param name="targetDir">Target directory that the song will be downloaded to</param>
         /// <param name="url">YouTube url of the song</param>
-        private void Start(string apikey, string targetDir, string url)
+        private void Start(string apikey, string url)
         {
             string youtube320 = "https://www.320youtube.com/watch?v=";
             API api = new API();
-            Downloader videoDownloader = new Downloader();
-            Files file = new Files();
-            string songTitle = "", downloadUrl = "", filePath = "", songFilePath = "";
-            Song songInfo = new Song();
-
+            string songTitle = "";
+            songInfo = new Song();
             try
             {
                 //Extract youtube ID from url
@@ -108,7 +115,7 @@ namespace YoutubeConverter
             {
                 string status = "Failed to get download url.";
                 UpdateStatusUI(status, true);
-                ShowError(status, error.ToString());
+                ShowError(status, error.Message.ToString());
                 return;
             }
             try
@@ -121,18 +128,31 @@ namespace YoutubeConverter
             {
                 string status = "Failed to get song information.";
                 UpdateStatusUI(status, true);
-                ShowError(status, error.ToString());
+                ShowError(status, error.Message.ToString());
                 return;
             }
 
-            UpdateSongInfo(songInfo);
-            // Wait for confirmation here
+            UpdateSongInfo();
+            UpdateStatusUI("Verify song information and press Download to continue.");
+            EnableFields(true);
+        }
 
+        /// <summary>
+        /// Downloads the song once the information is verified
+        /// </summary>
+        private void DownloadSong()
+        {
+            EnableFields(false);
 
-
-
-
-
+            Files file = new Files();
+            Downloader videoDownloader = new Downloader();
+            string songFilePath;
+            string filePath;
+            if (songInfo == null || targetDir == null || downloadUrl == null)
+            {
+                UpdateStatusUI("Song info error", true);
+                return;
+            }
             try
             {
                 UpdateStatusUI("Downloading song " + songInfo.Title + "...");
@@ -142,7 +162,7 @@ namespace YoutubeConverter
             {
                 string status = "Failed to download song";
                 UpdateStatusUI(status, true);
-                ShowError(status, error.ToString());
+                ShowError(status, error.Message.ToString());
                 return;
             }
             try
@@ -154,7 +174,7 @@ namespace YoutubeConverter
             {
                 string status = "Failed to move song";
                 UpdateStatusUI(status, true);
-                ShowError(status, error.ToString());
+                ShowError(status, error.Message.ToString());
                 return;
             }
             try
@@ -167,7 +187,7 @@ namespace YoutubeConverter
             {
                 string status = "Failed to update song properties";
                 UpdateStatusUI(status, true);
-                ShowError(status, error.ToString());
+                ShowError(status, error.Message.ToString());
             }
             try
             {
@@ -180,8 +200,27 @@ namespace YoutubeConverter
             {
                 string status = "Failed to remove backup file";
                 UpdateStatusUI(status, true);
-                ShowError(status, error.ToString());
+                ShowError(status, error.Message.ToString());
             }
+
+            songInfo = null;
+            downloadUrl = null;
+            targetDir = null;
+        }
+
+        /// <summary>
+        /// Enable or disable fields
+        /// </summary>
+        /// <param name="enable"></param>
+        private void EnableFields(bool enable)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                DownloadButton.IsEnabled = enable;
+                SongTitleTextBox.IsEnabled = enable;
+                ArtistTextBox.IsEnabled = enable;
+                AlbumTextBox.IsEnabled = enable;
+            });
         }
 
         /// <summary>
@@ -195,28 +234,76 @@ namespace YoutubeConverter
             MessageBox.Show(message + ": " + error);
         }
 
-        private void UpdateSongInfo(Song song)
+        /// <summary>
+        /// Updates the textbox for the song information received from api
+        /// </summary>
+        /// <param name="song"></param>
+        private void UpdateSongInfo()
         {
-            this.Dispatcher.Invoke(() =>
+            Dispatcher.Invoke(() =>
             {
-                SongTitleTextBox.Text = song.Title;
-                ArtistTextBox.Text = song.Artist;
-                AlbumTextBox.Text = song.Album;
+                SongTitleTextBox.Text = songInfo.Title;
+                ArtistTextBox.Text = songInfo.Artist;
+                AlbumTextBox.Text = songInfo.Album;
             });
         }
 
         private void OnConvertClick(object sender, RoutedEventArgs e)
         {
+            if (apiKeyTextBox.Text == "" || targetDirTextBox.Text == "" || urlTextBox.Text == "")
+            {
+                ShowError("Field Value Missing", "Enter a value into the appropriate field");
+                return;
+            }
             string apiKey = apiKeyTextBox.Text;
-            string targetDir = targetDirTextBox.Text;
+            targetDir = targetDirTextBox.Text;
             string url = urlTextBox.Text;
-            new Thread(() => Start(apiKey, targetDir, url)).Start();
+            new Thread(() => Start(apiKey, url)).Start();
         }
 
+        /// <summary>
+        /// Clears fields
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnClearClick(object sender, RoutedEventArgs e)
         {
             urlTextBox.Text = "";
             statusListBox.Items.Clear();
+            SongTitleTextBox.Text = "";
+            ArtistTextBox.Text = "";
+            AlbumTextBox.Text = "";
+        }
+
+        /// <summary>
+        /// Starts the download process
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnDownloadClick(object sender, RoutedEventArgs e)
+        {
+            new Thread(() => DownloadSong()).Start();
+        }
+
+        /// <summary>
+        /// Allows the user to import a text file containing urls
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnImportClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Shows the help window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnHelpClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Help help = new Help();
+            help.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            help.Show();
         }
     }
 }
